9f5a80018083977a700674c574f69bb62ad8ca54
GayathriHG <gai388926@gmail.com>
2023-12-21 01:52:05 +0530

    added new feature,subject options

--- DOM-tasks/Task-01-Form/assets/main.js
+++ DOM-tasks/Task-01-Form/assets/main.js
@@ -1,5 +1,25 @@
 
-//getting inputs using query selectors
-const userNameEl=document.querySelector(".yourName");
+//get from by name
+const formEl=document.forms.feedback;
 
-console.dir(userNameEl);
+// get button by queryselector
+const btnEl=document.querySelector("form button")
+
+
+
+ //get elements by object destructing
+
+const{fullName,email,subject,type,message}=formEl.elements;
+if(radioButton.checked) {
+    subject.value = labelName
+  }
+
+formEl.addEventListener ("submit",(event)=>{
+    event.preventDefault();
+
+    const formData=new FormData(formEl);
+    const{fullName,email,subject,type,message}=formEl.elements;
+  
+    console.log("form has been submitted");
+});
+
