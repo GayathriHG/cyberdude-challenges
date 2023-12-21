264486c0cdd51584f9aff6cbe0f00fb0697d1fda
GayathriHG <gai388926@gmail.com>
2023-12-21 15:13:21 +0530

    added readme file

---
+++ DOM-tasks/Task-01-Form/README.md
@@ -0,0 +1,12 @@
+# DOM-Task
+
+ ## Form
+
+This task was given as a part of free internship and skill development program by[CyberDude Networks Pvt. Ltd.](https://cyberdudenetworks.com).
+
+### live link
+[live link](https://gayathrihg.github.io/cyberdude-challenges/DOM-tasks/Task-01-Form/dist/).
+
+### Acknowledgement
+
+[AnbuSelvan](https://github.com/anburocky3).
--- DOM-tasks/Task-01-Form/assets/main.js
+++ DOM-tasks/Task-01-Form/assets/main.js
@@ -1,48 +1,29 @@
-
 //get from by name
-const formEl=document.forms.feedback;
-const typeEl=formEl.elements.type
-console.log([...typeEl]);
+const formEl = document.forms.feedback;
+const typeEl = formEl.elements.type;
+// console.log([...typeEl]);
 
 // get button by queryselector
-const btnEl=document.querySelector("form button")
-const optionsEl=document.querySelector("#options")
+const btnEl = document.querySelector("form button");
+const optionsEl = document.querySelector("#options");
 console.log(optionsEl);
 
+//get elements by object destructing
 
-
- //get elements by object destructing
-
-
-const handleSubmit=(event)=>{
+const handleSubmit = (event) => {
   event.preventDefault();
-  alert("Thank you for your feedback!")
+  alert("Thank you for your feedback!");
 
-  // const{fullName,email,subject,type,message}=formEl.elements;
+  const { fullName, email, subject, type, message } = formEl.elements;
 
   console.log(...new FormData(formEl));
-  
-
 };
+// getting radio input as subject input
+formEl.addEventListener("submit", handleSubmit);
 
-// const handleFormData=(e)=>{
+optionsEl.addEventListener("change", (e) => {
+  console.log(e.target.value);
 
-  // const formData=e.formData
-  // console.log([...formData.values()]);
-
-// }
-
-formEl.addEventListener ("submit",handleSubmit);
-
-// formEl.addEventListener("formdata",handleFormData);
-
-optionsEl.addEventListener ("change",(e)=>{
-console.log(e.target.value);
-
-const subjectEl=formEl.subject;
-subjectEl.value=e.target.value;
-
+  const subjectEl = formEl.subject;
+  subjectEl.value = e.target.value;
 });
- 
-
-
