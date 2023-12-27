cba81f8b51ce5cce50e54beedf561b7496413b4b
GayathriHG <gai388926@gmail.com>
2023-12-27 07:21:32 +0530

    resolved bug

--- DOM-tasks/01-form/assets/main.js
+++ DOM-tasks/01-form/assets/main.js
@@ -1,29 +1,37 @@
 //get from by name
 const formEl = document.forms.feedback;
 const typeEl = formEl.elements.type;
-// console.log([...typeEl]);
+
+
 
 // get button by queryselector
 const btnEl = document.querySelector("form button");
 const optionsEl = document.querySelector("#options");
-console.log(optionsEl);
 
 //get elements by object destructing
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  alert("Thank you for your feedback!");
 
   const { fullName, email, subject, type, message } = formEl.elements;
 
-  console.log(...new FormData(formEl));
+  const formData=new FormData(formEl);
+  formData.delete("type");
+  for(let entry of formData.entries()){
+    console.log(entry);
+  }
+
+
+
+  alert("Thank you for your feedback!");
+
 };
 // getting radio input as subject input
 formEl.addEventListener("submit", handleSubmit);
 
 optionsEl.addEventListener("change", (e) => {
-  console.log(e.target.value);
 
   const subjectEl = formEl.subject;
   subjectEl.value = e.target.value;
 });
+
