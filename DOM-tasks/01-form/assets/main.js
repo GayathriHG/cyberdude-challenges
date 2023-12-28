5fea3bd8fd2deae4d4cb513ad11b08d90573a1c8
GayathriHG <gai388926@gmail.com>
2023-12-28 13:48:16 +0530

    created ui

--- DOM-tasks/01-form/assets/main.js
+++ DOM-tasks/01-form/assets/main.js
@@ -2,8 +2,6 @@
 const formEl = document.forms.feedback;
 const typeEl = formEl.elements.type;
 
-
-
 // get button by queryselector
 const btnEl = document.querySelector("form button");
 const optionsEl = document.querySelector("#options");
@@ -14,29 +12,19 @@ const handleSubmit = (event) => {
   event.preventDefault();
 
   const { fullName, email, subject, message } = formEl.elements;
-
-  const formData=new FormData(formEl);
+// getting output in console
+  const formData = new FormData(formEl);
   formData.delete("type");
-  for(let entry of formData.entries()){
-  
+  for (let entry of formData.entries()) {
     console.log(...entry);
-
   }
 
-  
-
-
-
-
   alert("Thank you for your feedback!");
-
 };
 // getting radio input as subject input
 formEl.addEventListener("submit", handleSubmit);
 
 optionsEl.addEventListener("change", (e) => {
-
   const subjectEl = formEl.subject;
   subjectEl.value = e.target.value;
 });
-
