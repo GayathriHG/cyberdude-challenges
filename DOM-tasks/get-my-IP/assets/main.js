587cdfe329bd9abc8680b30d298fb04b847e574a
GayathriHG <gai388926@gmail.com>
2024-01-08 00:09:05 +0530

    created UI and git IP address

--- DOM-tasks/get-my-IP/assets/main.js
+++ DOM-tasks/get-my-IP/assets/main.js
@@ -1,6 +1,15 @@
+const ipAddressPlaceholderEl=document.querySelector("#ipAddress")
+
 const xhr=new XMLHttpRequest();
-xhr=onreadystatechange=function(){
-console.log(this);
+
+xhr.onreadystatechange=function(){
+    if(this.readyState === 4 && this.status === 200){
+        
+        ipAddressPlaceholderEl.textContent=xhr.responseText;
+    }
 };
-xhr.open("GET","")
+
+xhr.open("GET","https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/");
 xhr.send();
+
+
