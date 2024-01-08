c8f5278d3e78c67a8ca906dd61142f8d29a8e4dd
GayathriHG <gai388926@gmail.com>
2024-01-08 12:30:14 +0530

    added works session to UI

--- DOM-tasks/JSON-data/assets/main.js
+++ DOM-tasks/JSON-data/assets/main.js
@@ -2,5 +2,16 @@ import books from "../assets/books.json";
 const listEl = document.querySelector("#list");
 
 for (const author of books) {
-  listEl.innerHTML += `<li class="books-list">${author.name}</li>`;
+  listEl.innerHTML += `<div class="books-list" ><img src="${author.images}"><br><p>${author.name}</p> <p> ${author.age}</p><br>
+  <p>${author.about}</p><br>  <p><li>${author.works[0]}</li>
+  <li>${author.works[1]}</li>
+  <li>${author.works[2]}</li>
+  <li>${author.works[3]}</li>
+  <li>${author.works[4]}</li>
+  <li>${author.works[5]}</li>
+  <li>${author.works[6]}</li>
+  <li>${author.works[7]}</li>
+  <li>${author.works[8]}</li>
+  <li>${author.works[9]}</li></p></div>
+  `;
 }
