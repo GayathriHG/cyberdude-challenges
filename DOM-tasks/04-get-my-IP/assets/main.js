c0eb36a457fdfaa3fa0fa68e763c38907ad65334
GayathriHG <gai388926@gmail.com>
2024-01-08 17:34:51 +0530

    fix:changed file name

---
+++ DOM-tasks/04-get-my-IP/assets/main.js
@@ -0,0 +1,21 @@
+const ipAddressPlaceholderEl=document.querySelector("#ipAddress")
+
+const xhr=new XMLHttpRequest();
+
+xhr.onreadystatechange=function(){
+    if(this.readyState === 4 &&
+this.status === 200){
+ 
+        
+        ipAddressPlaceholderEl.textContent=xhr.responseText;
+
+    }else{
+        ipAddressPlaceholderEl.innerText="IP address not found";
+    }
+};
+
+
+xhr.open("GET","https://cors-anywhere.herokuapp.com/https://ipv4.icanhazip.com/");
+xhr.send();
+
+
