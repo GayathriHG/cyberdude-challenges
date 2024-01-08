c382b5714eed34d6c0c9199d0e6edbd02dd89ac5
GayathriHG <gai388926@gmail.com>
2024-01-08 17:33:58 +0530

    fix:changed file name

---
+++ DOM-tasks/03-JSON-data/assets/main.js
@@ -0,0 +1,17 @@
+import books from "../assets/books.json";
+const listEl = document.querySelector("#list");
+
+for (const author of books) {
+  listEl.innerHTML += `<div class="books-list" ><img src="${author.images}" width="120" height="120" class="rounded-full"><br><p>${author.name}</p> <p> ${author.age}</p><br>
+  <p>${author.about}</p><br>  <p><li>${author.works[0]}</li>
+  <li>${author.works[1]}</li>
+  <li>${author.works[2]}</li>
+  <li>${author.works[3]}</li>
+  <li>${author.works[4]}</li>
+  <li>${author.works[5]}</li>
+  <li>${author.works[6]}</li>
+  <li>${author.works[7]}</li>
+  <li>${author.works[8]}</li>
+  <li>${author.works[9]}</li></p></div>
+  `;
+}
