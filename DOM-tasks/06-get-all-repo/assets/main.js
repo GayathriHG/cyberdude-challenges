d79a049df10128e7d78269ffae815d48208d6630
GayathriHG <gai388926@gmail.com>
2024-01-10 23:13:50 +0530

    added style

--- DOM-tasks/06-get-all-repo/assets/main.js
+++ DOM-tasks/06-get-all-repo/assets/main.js
@@ -22,30 +22,24 @@ const interns = [
   "SwethaDSalvatore",
 ];
 
-// interns.forEach(username => {
-//     // console.log(name);
 
-//     const api=`https://api.github.com/users/${username}/repos`;
-//     console.log(api);
 
-// });
 
-// const imageEl=document.querySelector("#image");
-// document
-
+    
 interns.forEach((name) => {
-  const api = `https://api.github.com/users/${name}`;
-  // console.log(api);
+    const api = `https://api.github.com/users/${name}`;
+    // console.log(api);
+  
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const spanEl = document.createElement("span");
+        spanEl.innerHTML = `<div class="border p-6  rounded bg-white">
+  <img src="${data.avatar_url}" alt="profile image" class="w-12 h-12 rounded-full" id="image">
+  <h4 class="mt-2" id="name">${data.name}</h4>
+  <button class="bg-red-300 hover:bg-red-400 cursor-pointer rounded px-2 py-1 mt-2" id="button"><a href="${data.repos_url}"> View Repositories</a></button>
+  </div>`;
+        gitEl.append(spanEl);
+      });
+  });
 
-  fetch(api)
-    .then((response) => response.json())
-    .then((data) => {
-      const spanEl = document.createElement("div");
-      spanEl.innerHTML = `<div class="border p-6  rounded">
-<img src="${data.avatar_url}" alt="profile image" class="w-24 h-24 rounded-full" id="image">
-<h4 class="mt-2" id="name">${data.name}</h4>
-<button class="bg-white hover:bg-slate-300 cursor-pointer rounded px-2 py-1 mt-2" id="button"><a href="${data.repos_url}"> View Repositories</a></button>
-</div>`;
-      gitEl.append(spanEl);
-    });
-});
