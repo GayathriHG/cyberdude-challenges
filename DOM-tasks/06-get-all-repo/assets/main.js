85aee62c7cf254f42a92d3eaea85806eb0956449
GayathriHG <gai388926@gmail.com>
2024-01-10 11:35:20 +0530

    fix: show the repo in UI , able to get it in console

---
+++ DOM-tasks/06-get-all-repo/assets/main.js
@@ -0,0 +1,37 @@
+const gitEl=document.querySelector("#git")
+// console.log(gitEl);
+const interns=[
+    "GayathriHG",
+    "Jeya-rosini",
+    "kishorekumar-kp",
+    "Vk2401 ",
+    "yrd369",
+    "muthukumarimoorthi",
+    "vedhatech002",
+    "Bearcin46",
+    "mshajid",
+    "sharif-22",
+    "Mushkir",
+    "MUTHUAKALYA",
+    "esakki2104prsnl",
+    "SuriyaMassMsd",
+    "Riyaz1000",
+    "Hema-venkat3",
+    "dineshdevelope",
+    "danielace1",
+    "SwethaDSalvatore"
+]
+
+
+interns.forEach(name => {
+    // console.log(name);
+
+    const api=`https://api.github.com/users/${name}`;
+    // console.log(api);
+
+    
+});
+
+
+
+
