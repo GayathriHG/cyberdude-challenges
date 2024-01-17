39358e91c1624f3fa79546aa14adad36fef6a0dc
GayathriHG <gai388926@gmail.com>
2024-01-17 11:20:24 +0530

    init task 8 files

--- DOM-tasks/07-advice-API/assets/main.js
+++ DOM-tasks/07-advice-API/assets/main.js
@@ -1,12 +1,24 @@
 const sectionEl = document.querySelector("section");
 
-const quotes = new XMLHttpRequest();
-quotes.responseType = "json";
+const URL="https://api.adviceslip.com/advice"
 
-quotes.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    sectionEl.textContent = quotes.response.slip.advice;
-  }
-};
-quotes.open("GET", "https://api.adviceslip.com/advice");
-quotes.send();
+// const quotes = new XMLHttpRequest();
+// quotes.responseType = "json";
+
+// quotes.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     sectionEl.textContent = quotes.response.slip.advice;
+//   }
+// };
+// quotes.open("GET", "https://api.adviceslip.com/advice");
+// quotes.send();
+
+
+const result=fetch(URL).then((data)=>{
+  
+  
+console.log(jsonData);
+});
+console.log(result);
+
+
