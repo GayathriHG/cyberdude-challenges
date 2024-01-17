56236cec060a54b767627a9b08cd83dc20076e55
GayathriHG <gai388926@gmail.com>
2024-01-17 11:23:15 +0530

    changed color

--- DOM-tasks/07-advice-API/assets/main.js
+++ DOM-tasks/07-advice-API/assets/main.js
@@ -1,24 +1,24 @@
 const sectionEl = document.querySelector("section");
 
-const URL="https://api.adviceslip.com/advice"
+// const URL="https://api.adviceslip.com/advice"
 
-// const quotes = new XMLHttpRequest();
-// quotes.responseType = "json";
+const quotes = new XMLHttpRequest();
+quotes.responseType = "json";
 
-// quotes.onreadystatechange = function () {
-//   if (this.readyState == 4 && this.status == 200) {
-//     sectionEl.textContent = quotes.response.slip.advice;
-//   }
-// };
-// quotes.open("GET", "https://api.adviceslip.com/advice");
-// quotes.send();
+quotes.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    sectionEl.textContent = quotes.response.slip.advice;
+  }
+};
+quotes.open("GET", "https://api.adviceslip.com/advice");
+quotes.send();
 
 
-const result=fetch(URL).then((data)=>{
+// const result=fetch(URL).then((data)=>{
   
   
-console.log(jsonData);
-});
-console.log(result);
+// console.log(jsonData);
+// });
+// console.log(result);
 
 
