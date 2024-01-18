0fd4ca3083a02ec3f366643e770c76157eac3f87
GayathriHG <gai388926@gmail.com>
2024-01-18 15:22:27 +0530

    get values in console

--- DOM-tasks/08-split-calculator/assets/main.js
+++ DOM-tasks/08-split-calculator/assets/main.js
@@ -1,5 +1,9 @@
+const formEl = document.querySelector("form");
 const amountEl = document.querySelector("#amount");
 const peopleEl = document.querySelector("#people");
 const calculateEl = document.querySelector("#calculate");
 const resetEl = document.querySelector("#reset");
 const resultEl = document.querySelector("#result");
+const historyEl = document.querySelector("#history");
+
+
