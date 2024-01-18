b212ada58facb59af2a42dc2421226b30632fb98
GayathriHG <gai388926@gmail.com>
2024-01-18 13:40:59 +0530

    added history section

---
+++ DOM-tasks/08-split-calculator/assets/main.js
@@ -0,0 +1,5 @@
+const amountEl = document.querySelector("#amount");
+const peopleEl = document.querySelector("#people");
+const calculateEl = document.querySelector("#calculate");
+const resetEl = document.querySelector("#reset");
+const resultEl = document.querySelector("#result");
