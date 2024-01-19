7f445435f8e76338757e07bce3b1b731c3a809d8
GayathriHG <gai388926@gmail.com>
2024-01-19 11:35:03 +0530

    calculate share

--- DOM-tasks/08-split-calculator/assets/main.js
+++ DOM-tasks/08-split-calculator/assets/main.js
@@ -1,9 +1,25 @@
 const formEl = document.querySelector("form");
 const amountEl = document.querySelector("#amount");
+
 const peopleEl = document.querySelector("#people");
 const calculateEl = document.querySelector("#calculate");
 const resetEl = document.querySelector("#reset");
 const resultEl = document.querySelector("#result");
 const historyEl = document.querySelector("#history");
+
+
+
+
+calculateEl.addEventListener("click",(e)=>{
+    e.preventDefault();
+const amount=parseInt(amountEl.value);
+const people=parseInt(peopleEl.value);
+const share=amount/people;
+
+resultEl.textContent=`your share is: ₹
+${share}
+`
+
+});
 
 
