13e107a409edf2cb5dc6ef3567cbf5c65eaa0d4d
GayathriHG <gai388926@gmail.com>
2024-01-20 02:24:22 +0530

    got values in UI

--- DOM-tasks/08-split-calculator/assets/main.js
+++ DOM-tasks/08-split-calculator/assets/main.js
@@ -22,16 +22,75 @@ validateForm.addField("#amount", [
 
 validateForm.onSuccess(() => {
   const formData = new FormData(formEl);
+
   const formValueObj = Object.fromEntries(formData.entries());
+
+  const newBillSplitData = [];
+
+  const storedBillData = localStorage.getItem("splitBillData");
+
+  const storedBillArr = JSON.parse(storedBillData);
+
+  if (storedBillArr) {
+    storedBillArr.push(formValueObj);
+
+    localStorage.setItem("splitBillData", JSON.stringify(storedBillArr));
+  } else {
+    newBillSplitData.push(formValueObj);
+    localStorage.setItem("splitBillData", JSON.stringify(newBillSplitData));
+  }
+
+  // console.log(storedBillArr);
+
+  //   const splitBillObj = [];
+
+  //   splitBillObj.push(formValueObj);
+
+  //   localStorage.setItem("splitBillData", JSON.stringify(splitBillObj));
+
+  //   alert("Bill split successfully");
+  //   formEl.reset();
 });
+
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const amountEl = document.getElementById("amount").value;
-  console.log(amountEl);
+  // console.log(amountEl);
   const peopleEl = document.getElementById("people").value;
-  console.log(peopleEl);
+  // console.log(peopleEl);
   const splitEl = amountEl / peopleEl;
   console.log(splitEl);
-  const resultEl = document.getElementById("result");
 });
+
+function getAllBills() {
+  const splitBills = localStorage.getItem("splitBillData");
+  const splitBillsArr = JSON.parse(splitBills);
+
+  const historyEl = document.getElementById("history");
+  if (splitBillsArr && splitBillsArr.length > 0) {
+    historyEl.classList.remove("hidden");
+    const billTableEl = document.getElementById("billTable");
+    const finalBill = splitBillsArr
+      .map((splitBills) => {
+        return `
+            <tr class="rounded">
+            
+            <td class="px-2 py-1 border">${splitBills.amount}</td>
+            <td class="px-2 py-1 border">${splitBills.people}</td>
+
+            <td class="px-2 py-1 border"></td>
+           
+        </tr>
+            
+            `;
+      })
+      .join(" ");
+    billTableEl.innerHTML += finalBill;
+  } else {
+    historyEl.classList.add("hidden");
+  }
+
+  // console.log(splitBillsArr);
+}
+getAllBills();
