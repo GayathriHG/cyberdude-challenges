3cd15f821a96fcc5f6e7551a1231f4c2fb3653c0
GayathriHG <gai388926@gmail.com>
2024-01-25 19:26:17 +0530

    added header and footer to task 06

--- DOM-tasks/06-get-all-repo/assets/main.js
+++ DOM-tasks/06-get-all-repo/assets/main.js
@@ -24,17 +24,17 @@ const interns = [
 
 interns.forEach((name) => {
   const api = `https://api.github.com/users/${name}`;
-  // console.log(api);
+  console.log(api);
 
   fetch(api)
     .then((response) => response.json())
     .then((data) => {
       const spanEl = document.createElement("span");
-      spanEl.innerHTML = `<div class="border p-6  rounded bg-white">
-  <img src="${data.avatar_url}" alt="profile image" class="w-12 h-12 rounded-full" id="image">
-  <h4 class="mt-2" id="name">${data.name}</h4>
-  <button class="bg-red-300 hover:bg-red-400 cursor-pointer rounded px-2 py-1 mt-2" id="button"><a href="${data.repos_url}" target="blank"> View Repositories</a></button>
-  <button class="bg-red-300 hover:bg-red-400 cursor-pointer rounded px-2 py-1 mt-2" id="button"><a href="${data.html_url}" target="blank"> View Profile</a></button>
+      spanEl.innerHTML = `<div class="border p-6  rounded bg-slate-300 ">
+  <img src="${data.avatar_url}" alt="profile image" class="w-24 h-24 rounded-full " id="image">
+  <h4 class="mt-2 text-2xl font-semibold" id="name">${data.name}</h4>
+  <button class="bg-red-300 hover:bg-red-400 cursor-pointer rounded px-2 py-1 mt-2  text-xl" id="button"><a href="${data.html_url}" target="blank">  View Profile </a></button><br>
+  <button class="bg-red-300 hover:bg-red-400 cursor-pointer rounded px-2 py-1 mt-2 text-xl" id="button"><a href="${data.repos_url}" target="blank"> View Repositories</a></button>
 
   </div>`;
       gitEl.append(spanEl);
