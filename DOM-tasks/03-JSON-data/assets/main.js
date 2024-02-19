ce3e092257b721c81358ce6869dced25c400d03e
Gayathri H G <102051237+GayathriHG@users.noreply.github.com>
2024-02-19 16:59:57 +0530

    changed task 3 of dom

--- DOM-tasks/03-JSON-data/assets/main.js
+++ DOM-tasks/03-JSON-data/assets/main.js
@@ -3,14 +3,15 @@ const listEl = document.querySelector("#list");
 
 for (const author of books) {
   listEl.innerHTML += `<div class="books-list" >
-  <div class="flex  items-center ">
-  <div><img src="${author.images}"  ></div>
-  <div class="px-10">
+  <div class="flex">
+  
+  <div class="px-10 ">
   <div>${author.name}</div>
    <div> ${author.age}</div><br>
    <div>${author.about}</div>  
 
   </div>
+  <div ><img src="${author.images}"  ></div>
    </div><br>
   <div>
   <li>${author.works[0]}</li> 
@@ -23,6 +24,7 @@ for (const author of books) {
   <li>${author.works[7]}</li>
   <li>${author.works[8]}</li>
   <li>${author.works[9]}</li>
+  
   </div>
   </div>
   `;
